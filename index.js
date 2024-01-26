1ad891ff58d6acc260ad3c5f44c0cc6595ef8b71
David Honeyands <davidhoneyands@gmail.com>
2024-01-26 18:40:38 +0100

    Añado el atributo lorem a la clase A en el hotfix

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Clase genérica constructora con los atributos señalados
 class miClase{
-    constructor(foo,bar){
+    constructor(foo,bar,lorem){
         this.foo = foo;
         this.bar = bar;
+        this.lorem = lorem;
     }
 
 }
 
 // Clase A
-const ClaseA = new miClase ("Clase A foo","Clase A bar");
+const ClaseA = new miClase ("Clase A foo","Clase A bar","Clase A lorem");
 
 // Clase B
 const ClaseB = new miClase ("Clase B foo", "Clase B bar");
